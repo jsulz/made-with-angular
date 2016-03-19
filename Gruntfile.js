f10b95de0b7bda7c60626d8cb955dcc7e13d7d27
Jared Sulzdorf <jared@lexblog.com>
2016-03-18 22:03:05 -0700

    finished sass workflow setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,10 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass:{
 			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'none'
+				},
 				files: {
 					'style.css' : 'sass/style.scss'
 				}
